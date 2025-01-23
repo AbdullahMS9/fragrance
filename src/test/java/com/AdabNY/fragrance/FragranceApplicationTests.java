59bdbd4376e3371d3f99151e06f504bbe94ee1b7
AbdullahMS9 <65154836+AbdullahMS9@users.noreply.github.com>
2025-01-23 00:22:56 -0500

    Initial commit

---
+++ src/test/java/com/AdabNY/fragrance/FragranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdabNY.fragrance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
